303a5287a6632920f5dbb275c53cd812557814f3
phuuthanh-dev <phuuthanh2003@gmail.com>
2025-05-08 21:00:40 +0700

    fix

--- src/apis/studentAPI.js
+++ src/apis/studentAPI.js
@@ -4,3 +4,7 @@ import { BASE_URL } from "@/configs/globalVariables";
 export const getStudentAPI = async (page = 1 , size = 10, search = "") => {
   return await authorizedAxiosInstance.get(`${BASE_URL}/Students?page=${page}&size=${size}&search=${search}`);
 };
+
+export const getStudentScheduleAPI = async (year, weekNumber) => {
+  return await authorizedAxiosInstance.get(`${BASE_URL}/Students/schedule?year=${year}&week=${weekNumber}`);
+};
--- src/configs/globalVariables.js
+++ src/configs/globalVariables.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:5003/api/v1";
+const BASE_URL = "http://192.168.100.207:5003/api/v1";
 
 export { BASE_URL };
